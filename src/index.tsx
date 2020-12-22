b45308d6a1b18d1b592af953af0bdd84945d1db6
hila-abachi <igalk474@gmail.com>
2020-12-22 03:07:18 +0200

    add template files

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+ import React from 'react';
+ import { render } from 'react-dom';
+
+ import App from './App';
+ import './index.module.scss';
+
+render(
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>,
+   document.getElementById('root'),
+ );
+
+// Hot Module Replacement (HMR)
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
